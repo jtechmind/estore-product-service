df4d8e3c30c531997b5ba25e06a2cb8fd7935a5d
jtechmind <jtechmind@gmail.com>
2025-01-27 10:53:35 +0530

    Initialize estore-product-service with basic project setup

---
+++ src/main/java/com/jtechmind/estore_product_service/EstoreProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jtechmind.estore_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoreProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoreProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtechmind/estore_product_service/EstoreProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtechmind.estore_product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoreProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
