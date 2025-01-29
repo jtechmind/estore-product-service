1effc2fb1b84903cd2f3cdc453796e7d6a863e51
jtechmind <jtechmind@gmail.com>
2025-01-29 21:06:00 +0530

    feat: Enable service discovery with @EnableDiscoveryClient
    - Added @EnableDiscoveryClient annotation in EstoreProductServiceApplication.java.
    - Enables service registration and discovery for microservices architecture.
    - Improves scalability and inter-service communication.

--- src/main/java/com/jtechmind/estore_product_service/EstoreProductServiceApplication.java
+++ src/main/java/com/jtechmind/estore_product_service/EstoreProductServiceApplication.java
@@ -2,8 +2,10 @@ package com.jtechmind.estore_product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EstoreProductServiceApplication {
 
 	public static void main(String[] args) {
