afb5bc3a8484a2c980924a5d96d5fdd82842b71f
jtechmind <jtechmind@gmail.com>
2025-01-29 21:00:40 +0530

    feat: Add GlobalExceptionHandler for centralized error handling
    - Implemented GlobalExceptionHandler to manage exceptions across the application.
    - Added exception handling for ProductNotFoundException and other runtime errors.
    - Improved error response structure for better debugging and API consistency.

---
+++ src/main/java/com/jtechmind/estore_product_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.jtechmind.estore_product_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    // Handling validation errors
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    // Handling generic exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+}
